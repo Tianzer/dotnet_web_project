a2bddf44070b290672204b73aff286fdf08f0e2f
Tianze Bai <tbai@Riekerinc.com>
2017-06-30 13:15:56 -0400

    web page design: q1-q3

---
+++ web_demo1/Scripts/MyApp.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    // create a module
+    var app = angular.module('MyApp', ['ngRoute']); // use ng-route when implement routing
+    // create a controller
+    app.controller('HomeController', function ($scope) { // scope used for share data between view and controller
+        $scope.Message = "This is from MyApp ! ";
+
+
+    });
+})();
